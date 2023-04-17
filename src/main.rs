480842254daebbc70964cd2004e6a45f5b6fe8e0
Ivan Vučica <ivan@vucica.net>
2023-04-17 21:11:08 +0100

    Reformatting.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use tokio::io::{AsyncReadExt,AsyncWriteExt}; // adds new methods on TcpStream etc.
-use tokio::io::{AsyncBufRead,AsyncBufReadExt}; // allows for conn.lines()
+use tokio::io::{AsyncBufRead, AsyncBufReadExt};
+use tokio::io::{AsyncReadExt, AsyncWriteExt}; // adds new methods on TcpStream etc. // allows for conn.lines()
 
 async fn broadcast(name: &String, line: String) -> Result<(), String> {
     println!("<{name}> {line}");
@@ -9,12 +9,18 @@ async fn broadcast(name: &String, line: String) -> Result<(), String> {
 // TODO: implement broadcast
 
 async fn chat_conn(mut conn: tokio::net::TcpStream) -> Result<(), String> {
-    conn.write_all("Name? ".as_bytes()).await.map_err(|e| e.to_string())?;
+    conn.write_all("Name? ".as_bytes())
+        .await
+        .map_err(|e| e.to_string())?;
 
-    let buffered_reader =  tokio::io::BufReader::new(conn);
+    let buffered_reader = tokio::io::BufReader::new(conn);
     let mut line_reader = buffered_reader.lines();
 
-    let name = line_reader.next_line().await.map_err(|e| e.to_string())?.unwrap(); // TODO: fix panic by using unwrap_or_default() or similar
+    let name = line_reader
+        .next_line()
+        .await
+        .map_err(|e| e.to_string())?
+        .unwrap(); // TODO: fix panic by using unwrap_or_default() or similar
 
     while let Some(line) = line_reader.next_line().await.map_err(|e| e.to_string())? {
         broadcast(&name, line).await?;
@@ -32,7 +38,6 @@ async fn main() -> Result<(), String> {
     loop {
         let (conn, sockaddr) = listener.accept().await.map_err(|e| e.to_string())?;
         tokio::spawn(async { chat_conn(conn).await }); // in this line, ownership of conn passed into chat_conn() -- i.e. conn is "moved"
-        
     }
     return Ok(());
 }
